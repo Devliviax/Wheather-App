3357d6d56684332faf005e693fc6b00ee4c7714c
livia xavier <liviaxavier200317@gmail.com>
2024-09-21 19:36:32 -0300

    Initial Commit

---
+++ Wheather AppTests/Wheather_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Wheather_AppTests.swift
+//  Wheather AppTests
+//
+//  Created by livia on 21/09/24.
+//
+
+import Testing
+@testable import Wheather_App
+
+struct Wheather_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
